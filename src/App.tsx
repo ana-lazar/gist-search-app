a064044da7bbc49191bd11e59b895361df2647a1
ana-lazar <54860891+ana-lazar@users.noreply.github.com>
2022-09-23 19:30:34 +0300

    Import styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import FilterableGistList from "./gists/FilterableGistList";
+import styled from "styled-components";
+
+export const AppWrapper = styled.div``;
 
 function App() {
   return (
-    <>
+    <AppWrapper>
       <Header text="Welcome to Gist Searcher!" />
       <FilterableGistList />
-    </>
+    </AppWrapper>
   );
 }
 
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,20 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: black;
+  border-bottom: 2px solid black;
+  padding: 30px;
+  margin: 0px;
+`;
+
 interface HeaderProps {
   text: string;
 }
 
 const Header = ({ text }: HeaderProps) => {
-  return <div>{text}</div>;
+  return <StyledHeader>{text}</StyledHeader>;
 };
 
 export default Header;
